c6a056a5a38d54246a89933743e87fc0fa210236
JeFFBlanco <jefeblanco22@gmail.com>
2017-06-13 21:29:02 -0600

    Progress commit : start to craft story with tokenized word rhyme replacement TODO: - read config from file - gen verse 1, 2, and conclusion

--- API/word-api.js
+++ API/word-api.js
@@ -1,7 +1,9 @@
 const datamuse = require('./ThirdParty/datamuse-api.js');
 const dict = require('./ThirdParty/webdict-api.js');
 const _ = require('lodash');
-const helpers = require('../Utilities/helpers.js')
+const helpers = require('../Utilities/helpers.js');
+const rando = require('random-int');
+
 
 module.exports = {
 
@@ -13,13 +15,23 @@ module.exports = {
 		return datamuse.getRhymes(word, callback);
 	},
 
-	getIntro: function(wordData, rhymeTemplate) {
-		// Narrow down to phrases with correct part of speech.
-		var intros = _.find(rhymeTemplate.IntroPharases, function(p) { return p. })
-		// Grab random intro temple
+	getIntro: function(rhymes, introPhrases) {
+		return this.partOfSpeechReplace(rhymes, introPhrases);
+	},
 
+	partOfSpeechReplace: function(rhymes, phrases) {
+		
+		// Get random phrase.
+		var randoPhraseIndex = rando(0, phrases.length - 1);
+		var phrase = phrases[randoPhraseIndex];
 
-		//
+		// Get random rhyme for each part-of-speech, if possible.
+		var nouns = _.filter(rhymes, function(r) { return _.includes(r.tags, 'n'); });
+		var randoNounIndex = rando(0, nouns.length - 1);
+		var noun = nouns[randoNounIndex];
+
+		// TODO: Fill in dummy word if no rhymes availble for target part-of-speech
+
 	}
 
 };
--- rhyme-generator.js
+++ rhyme-generator.js
@@ -6,8 +6,16 @@ const wordApi = require('./API/word-api.js');
 var word = 'pond';
 wordApi.getRhymes(word, function(rhymes) {
 	console.log(util.format('Got some rhymes for %s', word));
+
+	generateRhymeyStory(word, rhymes);
 });
 
 wordApi.getDefinition(word, function(defn) {
 	console.log(util.format("Got defintion for %s.  Part of Speech is '%s'", word, defn.type));
 });
+
+
+function generateRhymeyStory(word) {
+	var intro = wordApi.getIntro(word);
+	// var verse1 = 
+}
