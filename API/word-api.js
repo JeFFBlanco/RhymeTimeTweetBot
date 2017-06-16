2db07b09104a31d8aa5c729327117baa116b3277
JeFFBlanco <jefeblanco22@gmail.com>
2017-06-15 21:07:41 -0600

    Rhymey Story code complete; time to TEST!

--- API/word-api.js
+++ API/word-api.js
@@ -1,8 +1,8 @@
 const datamuse = require('./ThirdParty/datamuse-api.js');
 const dict = require('./ThirdParty/webdict-api.js');
-const _ = require('lodash');
 const helpers = require('../Utilities/helpers.js');
 const rando = require('random-int');
+const _ = require('lodash');
 
 
 module.exports = {
@@ -20,18 +20,34 @@ module.exports = {
 	},
 
 	partOfSpeechReplace: function(rhymes, phrases) {
-		
 		// Get random phrase.
 		var randoPhraseIndex = rando(0, phrases.length - 1);
 		var phrase = phrases[randoPhraseIndex];
 
+		var phrase = this.partOfSpeechReplace(rhymes, phrase, 'n');
+		phrase = this.partOfSpeechReplace(rhymes, phrase, 'v');
+		phrase = this.partOfSpeechReplace(rhymes, phrase, 'adv');
+		phrase = this.partOfSpeechReplace(rhymes, phrase, 'adj');
+
+		return phrase;
+	},
+
+	// targetPartOfSpeech : 'n', 'v', 'adv', 'adj', etc..
+	partOfSpeechReplace: function(rhymes, phrase, targetPartOfSpeech) {
+			
 		// Get random rhyme for each part-of-speech, if possible.
-		var nouns = _.filter(rhymes, function(r) { return _.includes(r.tags, 'n'); });
-		var randoNounIndex = rando(0, nouns.length - 1);
-		var noun = nouns[randoNounIndex];
+		var potentialRhymes = _.filter(rhymes, function(r) { return _.includes(r.tags, targetPartOfSpeech); });
 
-		// TODO: Fill in dummy word if no rhymes availble for target part-of-speech
+		// Default word if no rhymes of that part-of-speech available.
+		var rhyme = { "word": "narwhal" };
+		if (potentialRhymes.length > 0) {
+			var randoIndex = rando(0, potentialRhymes.length - 1);
+			var rhyme = potentialRhymes[randoIndex];	
+		}
 
+		phrase = phrase.replace(targetPartOfSpeech, rhyme.word);
+
+		return phrase;
 	}
 
 };
--- rhyme-generator.js
+++ rhyme-generator.js
@@ -1,9 +1,16 @@
 
-var util = require('util');
+const util = require('util');
+const jsonFile = require('json-tu-file');
+const async = require('async');
 const wordApi = require('./API/word-api.js');
 
 
+
+// TODO: command line argument for word
 var word = 'pond';
+
+getPhraseTemplate();
+
 wordApi.getRhymes(word, function(rhymes) {
 	console.log(util.format('Got some rhymes for %s', word));
 
@@ -16,6 +23,39 @@ wordApi.getDefinition(word, function(defn) {
 
 
 function generateRhymeyStory(word) {
-	var intro = wordApi.getIntro(word);
-	// var verse1 = 
+	var intro = wordApi.getIntro(word, _phraseTemplate.IntroPharases);
+	
+	var transition1 = wordApi.getIntro(word, _phraseTemplate.Transitions);
+	var verse1 = wordApi.getIntro(word, _phraseTemplate.Verses);
+	
+	var transition2 = wordApi.getIntro(word, _phraseTemplate.Transitions);
+	var verse2 = wordApi.getIntro(word, _phraseTemplate.Verses);
+
+	var rhymeytimey = util
+						.formt('%s. \r\n', intro)
+						.formt('%s. \r\n', transition1)
+						.formt('%s. \r\n', verse1)
+						.formt('%s. \r\n', transition2)
+						.formt('%s. \r\n', verse2);
+
+	return rhymeytimey;
+}
+
+function getPhraseTemplate(callback) {
+    console.log('Initializing story phrases...........');
+
+    // TODO: Make Config file a commandline argument
+    jsonFile.readFile('.\\Templates\\rhyme-time.template.json', function (err, data) {
+        return handlePhraseTemplateLoaded(err, data, callback);
+    });
+}
+
+function handlePhraseTemplateLoaded(err, data, callback) {
+	if (err) {
+		console.log('Beep Bope Boop - Whoopsies! RhymeBot could not load story tempate') 
+		throw;
+	}
+	_phraseTemplate = data;
+
+	return callback();
 }
