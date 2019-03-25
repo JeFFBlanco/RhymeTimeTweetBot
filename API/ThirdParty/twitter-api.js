48c4c54ae3f69abab3ec3eaa8c08dd907c0d8561
JeFFBlanco <jwhite@commercesync.com>
2019-03-25 15:49:50 -0600

    yank de-authenticated items

--- API/ThirdParty/twitter-api.js
+++ API/ThirdParty/twitter-api.js
@@ -1,12 +1,6 @@
 var Twitter = require('twitter');
 
-// TODO: Protect these values.
-var client = new Twitter({
-	consumer_key: 'MhyFLgQ2jqE9nCbahWppa848R',
-	consumer_secret: 'euKxpLVlxUUNCfLUHHe0UaCN9m7CHipLd93KyF6kZOQlMuuoNw',
-	access_token_key: '877006820451262464-TI36NneDUjtWcYnlNT04GhlcvySevLr',
-	access_token_secret: 'HqcbUaAwNYej1gVEBEE08NOcM1bOXfgsGsDwxL2FS7gx2'
-});
+// Deez Be De-Authenticated
 
 module.exports = {
 
