b159d7381799017ae9108dd3ac090b9e1b3ed4c5
JeFFBlanco <jwhite@commercesync.com>
2019-03-25 15:50:23 -0600

    simplified call

--- API/ThirdParty/twitter-api.js
+++ API/ThirdParty/twitter-api.js
@@ -5,13 +5,7 @@ var Twitter = require('twitter');
 module.exports = {
 
 	tweet: function (status, callback) {
-		client.post('statuses/update', { status: status }, function (error, tweet, response) {
-			if (error) throw error;
-			// console.log(tweet);  // Tweet body.
-			// console.log(response);  // Raw response object.
-
-			callback(error, response);
-		});
+		callback();
 	}
 
 };
